67716b9ea155695427c6b52b93abd0786af91005
Stephen Haywood <stephen@averagesecurityguy.info>
2014-02-16 18:01:16 -0500

    Multiple DB updates.
    Added missing methods needed by lookup modules.
    Modified db schema to use id:<num> instead of <num> as the id key

--- alias/db.py
+++ alias/db.py
@@ -6,7 +6,6 @@ import redis
 import alias.config
 
 email_re = re.compile(r'.*@.*\..*')
-
 cfg = alias.config.AliasConfig()
 
 user_db = redis.StrictRedis(host='localhost', port=6379, db=cfg.user_db)
@@ -15,7 +14,7 @@ nym_db = redis.StrictRedis(host='localhost', port=6379, db=cfg.nym_db)
 loc_db = redis.StrictRedis(host='localhost', port=6379, db=cfg.loc_db)
 url_db = redis.StrictRedis(host='localhost', port=6379, db=cfg.url_db)
 name_db = redis.StrictRedis(host='localhost', port=6379, db=cfg.name_db)
-desc_db = redis.StrictRedis(host='localhost', port=6379, db=cfg.desc_db)
+about_db = redis.StrictRedis(host='localhost', port=6379, db=cfg.about_db)
 image_db = redis.StrictRedis(host='localhost', port=6379, db=cfg.image_db)
 
 
@@ -45,7 +44,34 @@ def get_all_targets():
     '''
     Return a list of all targets.
     '''
-    targets = [user_db.hget(i, 'key') for i in user_db.keys('*')]
+    targets = [user_db.hget(i, 'key') for i in user_db.keys('id:*')]
+    return targets
+
+
+def get_unchecked_targets(source, key_type):
+    '''
+    Return a list of targets that have not been looked up at the specified
+    source. In addition, make sure the key_type matches the requested type.
+    Some sources work well with usernames, others with emails, and others will
+    work with either.
+    '''
+    targets = []
+
+    if source not in cfg.valid_sources:
+        return targets
+
+    if key_type not in ['user', 'email', 'all']:
+        return targets
+
+    for tid in user_db.keys('id:*'):
+        user_type = user_db.hget(tid, 'type')
+
+        if (key_type != user_type) and (key_type != 'all'):
+            continue
+
+        if user_db.hget(tid, source) == '0':
+            targets.append(user_db.hget(tid, 'key'))
+
     return targets
 
 
@@ -53,10 +79,11 @@ def get_targets_with_data():
     '''
     Return a list of all targets that have data associated with them.
     '''
-    ids = set(email_db.keys('*'))
-    ids.union(nym_db.keys('*'), url_db.keys('*'))
-    ids.union(loc_db.keys('*'), name_db.keys('*'))
-    ids.union(desc_db.keys('*'), image_db.keys('*'))
+    # Create a set of all the ids in each 
+    ids = set(email_db.keys('id:*'))
+    ids.union(nym_db.keys('id:*'), url_db.keys('id:*'))
+    ids.union(loc_db.keys('id:*'), name_db.keys('id:*'))
+    ids.union(about_db.keys('id:*'), image_db.keys('id:*'))
 
     targets = [user_db.hget(i, 'key') for i in ids]
 
@@ -70,11 +97,22 @@ def add_new_target(target, key_type):
     key_id = user_db.get('key_id')
     user_db.incr('key_id')
 
-    data = {'key': target, 'type': key_type,
-            'gravatar': None, 'twitter': None}
+    data = {'key': target, 'type': key_type}
+    for source in cfg.valid_sources:
+        data[source] = '0'
 
-    user_db.hmset(key_id, data)
-    user_db.set(target, key_id)
+    tid = 'id:' + key_id
+    user_db.hmset(tid, data)
+    user_db.set(target, tid)
+
+
+def mark_source_complete(target, source):
+    '''
+    Change the value of the specified source from None to 1 to indicate that
+    this source has been checked for this user.
+    '''
+    tid = user_db.get(target)
+    user_db.hset(tid, source, '1')
 
 
 def get_target_data(target):
@@ -86,7 +124,7 @@ def get_target_data(target):
     data['urls'] = url_db.lrange(tid, 0, -1)
     data['locs'] = loc_db.lrange(tid, 0, -1)
     data['names'] = name_db.lrange(tid, 0, -1)
-    data['descs'] = desc_db.lrange(tid, 0, -1)
+    data['abouts'] = about_db.lrange(tid, 0, -1)
     data['images'] = image_db.lrange(tid, 0, -1)
 
     return data
@@ -119,7 +157,7 @@ def add_target_name(target, name):
 
 def add_target_description(target, desc):
     tid = user_db.get(target)
-    desc_db.lpush(tid, desc)
+    about_db.lpush(tid, desc)
 
 
 def add_target_image(target, url):
